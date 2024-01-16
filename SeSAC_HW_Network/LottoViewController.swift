5bafcad57baa3eb9794fba67c6fdd358056bbabb
jieun <jieun_0330@naver.com>
2024-01-16 17:32:12 +0900

    최근 회차 조회, 서버통신

--- SeSAC_HW_Network/LottoViewController.swift
+++ SeSAC_HW_Network/LottoViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import Alamofire
+
+struct Lotto: Decodable {
+    let drwtNo1: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let drwtNo6: Int
+    let bnusNo: Int
+}
 
 class LottoViewController: UIViewController {
 
     @IBOutlet var numberTextField: UITextField!
+    @IBOutlet var lottoNumberLabel: UILabel!
     
-    let numberList: [Int] = Array(1...1025).reversed()
+    let numberList: [Int] = Array(1...1102).reversed()
     
     var lottoPickerView = UIPickerView()
     
@@ -23,9 +35,29 @@ class LottoViewController: UIViewController {
         
         lottoPickerView.delegate = self
         lottoPickerView.dataSource = self
+        
+        callRequest()
 
     }
+    
+    func callRequest() {
+        
+        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1102"
+        AF
+            .request(url)
+            .responseDecodable(of: Lotto.self) { response in
+                switch response.result {
+                case .success(let success):
+                    self.lottoNumberLabel.text = "\(String(success.drwtNo1)) \(String(success.drwtNo2)) \(String(success.drwtNo3)) \(String(success.drwtNo4)) \(String(success.drwtNo5)) \(String(success.drwtNo6))"
+                case .failure(let failure):
+                    print("오류 발생")
+                }
+            }
+    }
+    
 }
+
+
 
 extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource  {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
